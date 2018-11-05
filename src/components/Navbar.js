a173a250cb47db128f112c0631b33f99249bb05a
Brocier <jschoe04@gmail.com>
2018-11-05 12:16:37 -0500

    Personal logo v.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/logo.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -11,7 +11,7 @@ const Navbar = () => (
           <figure className="image">
             <img
               src={logo}
-              alt="Kaldi"
+              alt="JSchoenfeld Logo"
               style={{
               width: '88px'
             }}/>
