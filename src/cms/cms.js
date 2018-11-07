cc99ed3080fdaba3a0719fe617f9e7983b84b668
Brocier <jschoe04@gmail.com>
2018-11-07 09:27:01 -0500

    Remove references to extinct Products page.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,8 +2,6 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import ProjectPostPreview from './preview-templates/ProjectPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('project', ProjectPostPreview)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,9 +23,6 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
         <Link className="navbar-item" to="/pets">
           Pets
         </Link>
