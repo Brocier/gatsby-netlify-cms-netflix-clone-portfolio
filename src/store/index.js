2ccd9dbf28fc42f49ef3a46e5cfda142da0f3f30
Brocier <jschoe04@gmail.com>
2018-11-12 10:17:18 -0500

    remove routerHistory from store for server side webpack bug fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
-import { routerMiddleware } from "react-router-redux";
 import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
 import thunk from "redux-thunk";
 import { createLogger } from "redux-logger";
-
 import rootReducer from "../reducers";
 
-export default function configureStore(history) {
-  // Apply navigation middleware
-  const routerHistory = routerMiddleware(history);
-
+export default function configureStore() {
   // Middleware for logging redux action to console
   const logger = createLogger({ collapsed: true });
 
   const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(...[thunk, routerHistory, logger]))
+    composeWithDevTools(applyMiddleware(...[thunk, logger]))
   );
 
   return store;
