2402ad78d14bf6d3929a7c5946a25a9bf2dbb3e7
Brocier <jschoe04@gmail.com>
2018-11-12 17:02:10 -0500

    Set all to be .scss instead of .sass so I can use @import syntax for bootstrap.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Helmet from "react-helmet";
 
 import Navbar from "../components/Navbar";
-import "./all.sass";
+import "./all.scss";
 import favicon from "../../static/img/icons/favicon.ico";
 
 const TemplateWrapper = ({ children }) => (
