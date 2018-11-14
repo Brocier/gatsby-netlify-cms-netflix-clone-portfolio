ff2d36b45a399d7890a0187ee1f8bf864307943f
Brocier <jschoe04@gmail.com>
2018-11-14 09:06:31 -0500

    Typo correction

--- src/components/Global/Footer/index.js
+++ src/components/Global/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="footer-container">
     <nav className="navbar navbar-light bg-faded">
       <a className="navbar-brand text-danger font-weight-bold" href="/">
-        Neflix
+        Netflix-Clone
       </a>
       <p className="text-muted lead" href="#">
         &copy;2018. All Rights Reserved by Chiu.
