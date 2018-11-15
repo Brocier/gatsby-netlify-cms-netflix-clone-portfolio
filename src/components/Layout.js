b74ad46959a611e6114e4d43f19820c7e4749472
Brocier <jschoe04@gmail.com>
2018-11-15 10:17:56 -0500

    Switch to React-strap and npm's bootstrap from locally installed assets.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import Helmet from "react-helmet";
 
 import Navbar from "../components/Navbar";
-import "./all.scss";
 import favicon from "../../static/img/icons/favicon.ico";
 
 const TemplateWrapper = ({ children }) => (
