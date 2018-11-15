d66f66ed4a2ec87b40ca5ee2e39f626bd2565895
Brocier <jschoe04@gmail.com>
2018-11-14 21:26:08 -0500

    Move all images to a single directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: `/netflixclone`,
   siteMetadata: {
     title: "JSchoenfeld Portfolio site"
   },
@@ -18,13 +19,6 @@ module.exports = {
       options: {
         path: `${__dirname}/src/pages`,
         name: "pages"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images"
       }
     },
     "gatsby-plugin-sharp",
