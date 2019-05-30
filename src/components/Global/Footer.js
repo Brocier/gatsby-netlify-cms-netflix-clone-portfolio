f7b221d42359d6025de55fd4c5754177ebc41e01
Brocier <jschoe04@gmail.com>
2019-05-30 16:51:53 -0400

    Switch OutboundLink to Link

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -13,12 +13,12 @@ const Footer = () => (
       <p className="p-0 m-0 text-muted lead" href="#">
         &copy;2018. All Rights Reserved by JSchoenfeld.
       </p>
-      <OutboundLink
+      <Link
         className="p-0 m-0 navbar-brand text-muted"
         href="https://github.com/Brocier/JSchoenfeld-Portfolio"
       >
         github
-      </OutboundLink>
+      </Link>
     </nav>
   </footer>
 );
